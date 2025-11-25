4aad8c1ed350818105aca9fcfc31e38a46df1d20
Thelmina4 <thelma.clarke24@mail.dcu.ie>
2025-11-25 20:10:59 +0000

    Remove the files

--- sem1/csc1038-sys-prog/labs/lab3/trying.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> // input output
-#include<string.h> // dealing with strings
-#include<stdbool.h> // booleans
-
-int main(int argc, char* argv[]) {
-    char* line = argv[1]
-    
-}

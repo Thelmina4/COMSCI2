1ffdc4db88e8b1b58df2470554095ddfc7978ad6
Thelmina4 <thelma.clarke24@mail.dcu.ie>
2026-04-02 14:10:30 +0100

    simpleshell

--- sem2/csc1031_java/lab3/Network.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Network {
-
-
-    public class Network {
-        // : Manages all towers, operators, and clients.
-    }
-}
--- sem2/csc1031_java/wk2/ForLoopExample.java
+++
@@ -1,9 +0,0 @@
-// ForLoopExample.java
-
-public class ForLoopExample {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Count: " + i);
-        }
-    }
-}
